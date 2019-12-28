19b97f08bbf1b5eaab8b413b4a3415a8d3cc0956
Do Thien Phu <huyenptth1907010@fpt.edu.vn>
2019-12-28 22:08:46 +0700

    CodeLearn

---
+++ Codelean/CodeleanA.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Codelean;
+
+/**
+ *
+ * @author Admin
+ */
+class CodeleanA {
+
+    public void methodPublic() {
+        methodPrivate();
+    }
+
+    protected void methodProtected() {
+        methodPrivate();
+    }
+
+    void methodDefault() {
+        methodPrivate();
+    }
+
+    private void methodPrivate() {
+
+    }
+
+}
---
+++ Codelean/CodeleanB.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Codelean;
+import Codelean.CodeleanA;
+/**
+ *
+ * @author Admin
+ */
+public class CodeleanB {
+    public static void main(String[] args) {
+        new CodeleanA().methodPublic();
+        new CodeleanA().methodProtected();
+        new CodeleanA().methodDefault();
+        
+    }
+    public  void methodPublic(){
+        
+    }
+    public  void methodProtected(){
+        
+    }
+    void methodDefault(){
+        
+    }
+    private void methodPrivate(){
+        
+    }
+}
