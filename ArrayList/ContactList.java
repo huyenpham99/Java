7c86636d81d28f827981e67d7c77262c6b0a1b03
Do Thien Phu <huyenptth1907010@fpt.edu.vn>
2020-01-05 23:36:00 +0700

    Update ContactList.java

--- ArrayList/ContactList.java
+++ ArrayList/ContactList.java
@@ -1,10 +1,10 @@
-import PhoneNumbers.Contact;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class ContactList {
+
     String name;
     String numberphone;
     static List<ContactList> contact = new ArrayList<>();
@@ -12,6 +12,14 @@ public class ContactList {
     public ContactList(String name, String numberphone) {
         this.name = name;
         this.numberphone = numberphone;
+    }
+
+    public static List<ContactList> getContact() {
+        return contact;
+    }
+
+    public static void setContact(List<ContactList> contact) {
+        ContactList.contact = contact;
     }
 
     public ContactList() {
@@ -52,6 +60,16 @@ public class ContactList {
 
     }
 
+//    public int searchContact(String name) {
+//        for (int i = 0; i < contact.size(); i++) {
+//            ContactList contactList = this.contact.get(i);
+//            if (ContactList.getName().equals(name)){
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+
 
     public void display() {
         System.out.println("You have" + contact.size() + "items in list");
@@ -79,57 +97,61 @@ public class ContactList {
         System.out.println("4-To remove");
         System.out.println("5-To search");
         System.out.println("6-To print all option");
+        System.out.println("----- ----- -----");
+        System.out.print("Chose: ");
         int chose;
         Scanner input = new Scanner(System.in);
-        System.out.println("Your Chose:");
         chose = input.nextInt();
+        do {
+            switch (chose) {
 
-        switch (chose) {
-            case 0:
-
-                System.out.println("0-To shutdown");
-                break;
-            case 1:
-                System.out.println("1-To print contact");
-                obj.display();
-                break;
-            case 2:
-                System.out.println("2-To add ");
-                System.out.println("Moi ban nhap ten va sdt users:");
-                obj.addcontactList(input.next(), input.next());
-                obj.display();
-                break;
-            case 3:
-                System.out.println("3-To update");
-                System.out.println("Moi ban nhap thong tin can update gom co index, name, sdt:");
-                obj.updateContact(input.nextInt(), input.next(), input.next());
-                obj.display();
-                break;
-            case 4:
-                System.out.println("4-To remove");
-                System.out.println("Moi ban nhap thong tin can remove: index");
-                obj.removeContact(input.nextInt());
-                obj.display();
-                break;
-            case 5:
-                System.out.println("5-To find");
-                System.out.println("Moi ban nhap thong tin can tim kiem");
+                case 1:
+                    System.out.println("1-To print contact");
+                    obj.display();
+                    break;
+                case 2:
+                    System.out.println("2-To add ");
+                    System.out.println("Moi ban nhap ten va sdt users:");
+                    obj.addcontactList(input.next(), input.next());
+                    obj.display();
+                    break;
+                case 3:
+                    System.out.println("3-To update");
+                    System.out.println("Moi ban nhap thong tin can update gom co index, name, sdt:");
+                    obj.updateContact(input.nextInt(), input.next(), input.next());
+                    obj.display();
+                    break;
+                case 4:
+                    System.out.println("4-To remove");
+                    System.out.println("Moi ban nhap thong tin can remove: index");
+                    obj.removeContact(input.nextInt());
+                    obj.display();
+                    break;
+                case 5:
+                    System.out.println("5-To find");
+                    System.out.println("Moi ban nhap thong tin can tim kiem");
 
 
-                break;
-            case 6:
-                System.out.println("6-To print all option");
-                obj.addcontactList(input.next(), input.next());
-                obj.updateContact(input.nextInt(), input.next(), input.next());
-                obj.removeContact(input.nextInt());
-                obj.display();
-                break;
-            default:
-                System.out.println("Unvailable! Chose other!");
-                break;
+                    break;
+                case 6:
+                    System.out.println("6-To print all option");
+                    obj.addcontactList(input.next(), input.next());
+                    obj.updateContact(input.nextInt(), input.next(), input.next());
+                    obj.removeContact(input.nextInt());
+                    obj.display();
+                    break;
+                case 0:
+                    System.out.println("0-To shutdown");
+                    break;
+                default:
+                    System.out.println("Unvailable! Chose other!");
+                    break;
 
 
-        }
+            }
+
+        } while (chose != 0);
+
 
     }
 
